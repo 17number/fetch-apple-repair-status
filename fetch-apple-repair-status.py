d3c64dfd5e286a41668f461fae1fd65ca64ed875
r17n <4716989+17number@users.noreply.github.com>
2019-09-28 14:51:00 +0900

    feat: add link to repair detail page

--- fetch-apple-repair-status.py
+++ fetch-apple-repair-status.py
@@ -6,6 +6,7 @@ APPLE_SUPPORT_API_BASE = f"{APPLE_SUPPORT_BASE}/api"
 APPLE_SUPPORT_API_V1 = f"{APPLE_SUPPORT_API_BASE}/v1"
 CSRF_API = f"{APPLE_SUPPORT_BASE}/csrf"
 REPAIR_API = f"{APPLE_SUPPORT_API_V1}/supportaccount/getRepairStatus"
+REPAIR_DETAIL = f"{APPLE_SUPPORT_BASE}/repairs/details"
 LINE_NOTIFY_URL = "https://notify-api.line.me/api/notify"
 
 def get_jsessionid():
@@ -18,13 +19,15 @@ def get_repair_status(repair_id, serial_number, jsessionid):
   res = requests.get(REPAIR_API, params=queries, headers=headers)
   return res.json()
 
-def extract_repair_summary(repair_status):
+def extract_repair_summary(repair_id, serial_number, repair_status):
   from datetime import datetime
   date = datetime.fromtimestamp(repair_status["data"]["repairMetaData"]["modifiedDate"] / 1000)
   msg  = "\n"
   msg += f"{date}\n"
   msg += f"{repair_status['data']['repairMetaData']['repairStatusDesc']}\n"
   msg += f"{repair_status['data']['repairMetaData']['repairShortDesc']}\n"
+  repair_detail = f"{REPAIR_DETAIL}/{repair_id}?serialNumber={serial_number}"
+  msg += f"{repair_detail}\n"
   return msg
 
 def get_repair_params():
@@ -44,7 +47,7 @@ if __name__ == '__main__':
   repair_id, serial_number = get_repair_params()
   jsessionid = get_jsessionid()
   repair_status = get_repair_status(repair_id, serial_number, jsessionid)
-  repair_summary = extract_repair_summary(repair_status)
+  repair_summary = extract_repair_summary(repair_id, serial_number, repair_status)
   notify_to_line(repair_summary)
 
   # IF you check all of response, comment out below codes.
